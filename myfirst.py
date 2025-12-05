1bacbf13ed07d976b2b61443ad6033ce542af795
Clo402 <prakhar.s.purohit@gmail.com>
2025-12-04 20:29:55 -0500

    Add files via upload
    This is my first project.

---
+++ myfirst.py
@@ -0,0 +1,10 @@
+#Print a Sentence
+print("Welcome to the world of programing!")
+#Print a Number
+print(11)
+#Next line after print
+print(" Hello Python \:n")
+#Pass multiple arguments for printing
+print("Hello",14)
+#End argument of Print Statement
+print("Welcome to", end="*")
