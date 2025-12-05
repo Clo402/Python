a09787f30cc8b5c67687e822ca0136c6a3b1d3a3
Clo402 <prakhar.s.purohit@gmail.com>
2025-12-04 20:40:31 -0500

    Add files via upload
    Second one

---
+++ variable.py
@@ -0,0 +1,10 @@
+#Creating different variables
+x=5
+y="Jhon"
+print(x)
+print(y)
+codingal="codingal"
+print(codingal)
+#User input
+name=input("Prakhar")
+print("\nHello",name,"\nwelcome to codingal")
