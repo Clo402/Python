de9ec4bde6018c5fdeb4702995b0090a84b43049
Clo402 <prakhar.s.purohit@gmail.com>
2025-12-15 20:00:14 -0500

    Add files via upload
    Number 6!

---
+++ reversestring.py
@@ -0,0 +1,8 @@
+#Input a word
+text=str(input("Enter a string:"))
+#Reverse String
+#Using step value as -1 to iterate in reverse
+revText=text[::-1]
+text=revText
+print("Reverse of given string is:")
+print(text)
