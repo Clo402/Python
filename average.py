a5150d24f669f2cce5fe7fd094d648a8a502fdf4
Clo402 <prakhar.s.purohit@gmail.com>
2025-12-18 20:23:14 -0500

    Add files via upload
    IDK I lost count

---
+++ average.py
@@ -0,0 +1,12 @@
+#Storing values
+tree1=98
+tree2=94
+tree3=41
+tree4=95
+tree5=11
+#Finding thr total of all the trees
+sum=tree1+tree2+tree3+tree4+tree5
+print("The sum of all the trees is:",sum)
+#Fining the average of all the trees
+average=sum/5
+print("The average of all the trees is:",average)
