d9ceb352dee14f00c2704b7d509990ab86aa1838
Clo402 <prakhar.s.purohit@gmail.com>
2025-12-22 11:38:56 -0500

    Add files via upload
    8th I Think?

---
+++ checkingthenumbergreaterorsmaller.py
@@ -0,0 +1,8 @@
+i=int(input("Enter a number:"))
+if (i<15):
+    print("i is smaller than 15")
+    print("I'm an IF Block")
+else:
+    print("i is bigger than 15")
+    print("I'm an Else Block")
+    print("I'm not an IF Block and not an Else Block")
