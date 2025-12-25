3ce056ce2995ec8291576c134b8c571b1cdde0b2
Clo402 <prakhar.s.purohit@gmail.com>
2025-12-25 13:14:00 -0500

    Add files via upload
    First Project!

---
+++ storingbirthdays.py
@@ -0,0 +1,13 @@
+#Storing Birthdays
+raj="Dec 4, 2014"
+jack="April 17, 2016"
+billy="Feb 29, 2014"
+bob="May 28, 2015"
+bart="June 30, 2013"
+alex="Oct 14, 2019"
+print("Raj's birthday is on",raj)
+print("Jack's birthday is on",jack)
+print("Billy's birthday is on",billy)
+print("Bob's birthday is on",bob)
+print("Bart's birthday is on",bart)
+print("Alex's birthday is on",alex)
