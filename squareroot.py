adb44572add809a2c11feae1282791c8408c8231
Clo402 <prakhar.s.purohit@gmail.com>
2025-12-25 14:03:03 -0500

    Add files via upload
    Top 3!

---
+++ squareroot.py
@@ -0,0 +1,5 @@
+#Square Root
+import math
+number = int(input("Please enter the number you wish to square root:"))
+square_root = math.sqrt(number)
+print(f"The square root of {number} is {square_root}")
