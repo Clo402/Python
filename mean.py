31cdbc09ffaea36e072fb0058500b2f19eeee589
Clo402 <prakhar.s.purohit@gmail.com>
2026-01-15 20:46:55 -0500

    Add files via upload
    Meanmean)(

---
+++ mean.py
@@ -0,0 +1,13 @@
+mean1=38
+wrong_number=36
+correct_number=56
+total_number=40
+#Sum of 40 numbers
+sum=mean1*total_number
+print("Sum of 40 numbers is: ",sum)
+#correct sum of these numbers
+num2=sum-((wrong_number)-(correct_number))
+print("sum-((wrong_number-(correct_number)): ",num2)
+#The correct mean of these numbers
+mean2=num2/total_number
+print("The correct mean of these numbers is: ",mean2)
